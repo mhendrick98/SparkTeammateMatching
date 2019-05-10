7e80e2b4a001d7c2c26cae06f2e175335137e890
mhendrick98 <mhendric@bu.edu>
2019-05-10 13:52:03 -0400

    done

--- revised/sparkx_fellows_students_matching.py
+++ revised/sparkx_fellows_students_matching.py
@@ -35,84 +35,19 @@ def main(args):
 	df_students = pd.read_csv('xlab_sparkx.csv')
 	df_students = df_students.fillna(0)
 
-	# 0th group of fellows
-	df_fellows_selected_columns_team_0 = pd.concat([df_fellows.ix[0:0,1], df_fellows.ix[0:0,2:]],axis=1)
-	print(df_fellows_selected_columns_team_0['Email'])
-	# 1st group of fellows
-	df_fellows_selected_columns_team_1 = pd.concat([df_fellows.ix[1:1,1], df_fellows.ix[1:1,2:]],axis=1)
-	# 2nd group of fellows
-	df_fellows_selected_columns_team_2 = pd.concat([df_fellows.ix[2:2,1], df_fellows.ix[2:2,2:]],axis=1)
-	# 3rd group of fellows
-	df_fellows_selected_columns_team_3 = pd.concat([df_fellows.ix[3:3,1], df_fellows.ix[3:3,2:]],axis=1)
-	# 4th group of fellows
-	df_fellows_selected_columns_team_4 = pd.concat([df_fellows.ix[4:4,1], df_fellows.ix[4:4,2:]],axis=1)
-	# 5th group of fellows
-	df_fellows_selected_columns_team_5 = pd.concat([df_fellows.ix[5:5,1], df_fellows.ix[5:5,2:]],axis=1)
-	# 6th group of fellows
-	df_fellows_selected_columns_team_6 = pd.concat([df_fellows.ix[6:6,1], df_fellows.ix[6:6,2:]],axis=1)
-	# 7th group of fellows
-	df_fellows_selected_columns_team_7 = pd.concat([df_fellows.ix[7:7,1], df_fellows.ix[7:7,2:]],axis=1)
+	num_of_fellows = 9
 
-
-	df_students_selected_columns = pd.concat([df_students.ix[:,2], df_students.ix[:,3:-7]],axis=1)
-	df_students_selected_column_names = df_students_selected_columns.columns.values
-
-
-
-	feature_vector_fellows_team_0 = create_feature_vector_of_fellows(df_fellows_selected_columns_team_0)
-	print('Length of feature vector fellows team 0:',len(feature_vector_fellows_team_0))
-	feature_vector_fellows_team_1 = create_feature_vector_of_fellows(df_fellows_selected_columns_team_1)
-	print('Length of feature vector fellows team 1:',len(feature_vector_fellows_team_1))
-	feature_vector_fellows_team_2 = create_feature_vector_of_fellows(df_fellows_selected_columns_team_2)
-	print('Length of feature vector fellows team 2:',len(feature_vector_fellows_team_2))
-	feature_vector_fellows_team_3 = create_feature_vector_of_fellows(df_fellows_selected_columns_team_3)
-	print('Length of feature vector fellows team 3:',len(feature_vector_fellows_team_3))
-	feature_vector_fellows_team_4 = create_feature_vector_of_fellows(df_fellows_selected_columns_team_4)
-	print('Length of feature vector fellows team 4:',len(feature_vector_fellows_team_4))
-	feature_vector_fellows_team_5 = create_feature_vector_of_fellows(df_fellows_selected_columns_team_5)
-	print('Length of feature vector fellows team 5:',len(feature_vector_fellows_team_5))
-	feature_vector_fellows_team_6 = create_feature_vector_of_fellows(df_fellows_selected_columns_team_6)
-	print('Length of feature vector fellows team 6:',len(feature_vector_fellows_team_6))
-	feature_vector_fellows_team_7 = create_feature_vector_of_fellows(df_fellows_selected_columns_team_7)
-	print('Length of feature vector fellows team 7:',len(feature_vector_fellows_team_7))
-
-	for index, row in df_students_selected_columns.iterrows():
-		student_id_features_dict[row['Email']] = row.tolist()[1:]
-
-	print('Computing score for team 0.')
-	student_score_team_0_list = compute_each_student_scores(student_id_features_dict,feature_vector_fellows_team_0)
-	print('Computing score for team 1.')
-	student_score_team_1_list = compute_each_student_scores(student_id_features_dict,feature_vector_fellows_team_1)
-	print('Computing score for team 2.')
-	student_score_team_2_list= compute_each_student_scores(student_id_features_dict,feature_vector_fellows_team_2)
-	print('Computing score for team 3.')
-	student_score_team_3_list = compute_each_student_scores(student_id_features_dict,feature_vector_fellows_team_3)
-	print('Computing score for team 4.')
-	student_score_team_4_list = compute_each_student_scores(student_id_features_dict,feature_vector_fellows_team_4)
-	print('Computing score for team 5.')
-	student_score_team_5_list = compute_each_student_scores(student_id_features_dict,feature_vector_fellows_team_5)
-	print('Computing score for team 6.')
-	student_score_team_6_list = compute_each_student_scores(student_id_features_dict,feature_vector_fellows_team_6)
-	print('Computing score for team 7.')
-	student_score_team_7_list = compute_each_student_scores(student_id_features_dict,feature_vector_fellows_team_7)
-
-
-	print('Writing selections for team 0.')
-	write_tuple_list_to_csv('fellow1.csv',student_score_team_0_list)
-	print('Writing selections for team 1.')
-	write_tuple_list_to_csv('fellow2.csv',student_score_team_1_list)
-	print('Writing selections for team 2.')
-	write_tuple_list_to_csv('fellow3.csv',student_score_team_2_list)
-	print('Writing selections for team 3.')
-	write_tuple_list_to_csv('fellow4.csv',student_score_team_3_list)
-	print('Writing selections for team 4.')
-	write_tuple_list_to_csv('fellow5.csv',student_score_team_4_list)
-	print('Writing selections for team 5.')
-	write_tuple_list_to_csv('fellow5.csv',student_score_team_5_list)
-	print('Writing selections for team 6.')
-	write_tuple_list_to_csv('fellow6.csv',student_score_team_6_list)
-	print('Writing selections for team 7.')
-	write_tuple_list_to_csv('fellow7.csv',student_score_team_7_list)
+	for i in range(0, num_of_fellows):
+		temp_fellows_selected_columns_team = pd.concat([df_fellows.ix[i:i,1], df_fellows.ix[i:i,2:]],axis=1)
+		df_students_selected_columns = pd.concat([df_students.ix[:,2], df_students.ix[:,3:-7]],axis=1)
+		df_students_selected_column_names = df_students_selected_columns.columns.values
+		temp_feature_vector_fellows_team = create_feature_vector_of_fellows(temp_fellows_selected_columns_team)
+		for index, row in df_students_selected_columns.iterrows():
+			student_id_features_dict[row['Email']] = row.tolist()[1:]
+		student_score_team_temp_list = compute_each_student_scores(student_id_features_dict,temp_feature_vector_fellows_team)
+		team_name = df_fellows.ix[i:i,0].values[0]
+		file_name = team_name + " Scores.csv"
+		write_tuple_list_to_csv(file_name,student_score_team_temp_list)
 
 
 if __name__ == '__main__':
